e9169c44c20c04f0c95705b1d598e8f73ec60625
PauloHenriqueVianna <paulo@note-pronatec.(none)>
2013-10-24 20:57:44 -0200

    Update views.py in mapagro app

--- mapprf/views.py
+++ mapprf/views.py
@@ -2,7 +2,8 @@
 from django.db.models import Count
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from mapprf.models import Ocorrencias, PrfRodovias
+from django.contrib.gis.gdal import SpatialReference,CoordTransform
+from mapprf.models import Ocorrencias, PrfRodovias, PrfRodovias
 
 
 def ocorrenciasBrasil(request,tipo,cod):
@@ -17,15 +18,15 @@ def ocorrenciasBrasil(request,tipo,cod):
 	hora = ocorrencias.extra(select={'nome':'extract(hour from data)'}).values('nome').order_by().annotate(valor=Count('data'))
 
 	for d in diaDaSemana:
-		porc = (qtOcorrencias * d['valor']) / 100
+		porc = (100 * d['valor']) / qtOcorrencias
 		d['nome'] = d['id_dia_semana__dia_da_semana']
 		d['porcentagem'] = porc
 	for m in mes:
-		porc = (qtOcorrencias * m['valor']) / 100
+		porc = (100 * d['valor']) / qtOcorrencias
 		m['porcentagem'] = porc
 		m['nome'] = getMes(m['nome'])
 	for h in hora:
-		porc = (qtOcorrencias * h['valor']) / 100
+		porc = (100 * d['valor']) / qtOcorrencias
 		h['porcentagem'] = porc
 		h['nome'] = int(h['nome'])
 
@@ -38,6 +39,10 @@ def ocorrenciasBrasil(request,tipo,cod):
 
 
 def ocorrenciasSegmento(request,cod):
+	rodovia = Rodovias.objects.filter(codigo__contains='386BR')
+	ct = CoordTransform(SpatialReference('EPSG:4326'), SpatialReference('EPSG:900913'))
+	for p in rodovia:
+		p.geometry.transform(ct)
 	segmento = PrfRodovias.objects.get(id=cod)
 	ocorrencias = Ocorrencias.objects.filter(id_local__br=386,id_local__km__range=(segmento.kmi,segmento.kmf))
 	qtOcorrencias = ocorrencias.count()
@@ -47,15 +52,15 @@ def ocorrenciasSegmento(request,cod):
 	hora = ocorrencias.extra(select={'nome':'extract(hour from data)'}).values('nome').order_by().annotate(valor=Count('data'))
 
 	for d in diaDaSemana:
-		porc = (qtOcorrencias * d['valor']) / 100
+		porc = (100 * d['valor']) / qtOcorrencias
 		d['nome'] = d['id_dia_semana__dia_da_semana']
 		d['porcentagem'] = porc
 	for m in mes:
-		porc = (qtOcorrencias * m['valor']) / 100
+		porc = (100 * d['valor']) / qtOcorrencias
 		m['porcentagem'] = porc
 		m['nome'] = getMes(m['nome'])
 	for h in hora:
-		porc = (qtOcorrencias * h['valor']) / 100
+		porc = (100 * d['valor']) / qtOcorrencias
 		h['porcentagem'] = porc
 		h['nome'] = int(h['nome'])
 
@@ -64,7 +69,42 @@ def ocorrenciasSegmento(request,cod):
                                                       'mortes': mortes,
                                                       'diaDaSemana':diaDaSemana,
                                                       'mes':mes,
-                                                      'hora':hora}))
+                                                      'hora':hora,
+                                                      'rodovia':rodovia}))
+
+
+def ocorrenciasRodovia(request,cod):
+	rodovia = Rodovias.objects.filter(codigo__contains='386BR')
+	ct = CoordTransform(SpatialReference('EPSG:4326'), SpatialReference('EPSG:900913'))
+	for p in rodovia:
+		p.geometry.transform(ct)
+	ocorrencias = Ocorrencias.objects.filter(id_local__br=cod)
+	qtOcorrencias = ocorrencias.count()
+	mortes = ocorrencias.filter(ocorrenciapessoa__id_pessoa__id_estado_fisico=4).count()
+	diaDaSemana = ocorrencias.extra(select={'nome':'id_dia_semana'}).values('nome','id_dia_semana__dia_da_semana').order_by().annotate(valor=Count('id_dia_semana'))
+	mes = ocorrencias.extra(select={'nome':'extract(month from data)'}).values('nome').order_by().annotate(valor=Count('data'))
+	hora = ocorrencias.extra(select={'nome':'extract(hour from data)'}).values('nome').order_by().annotate(valor=Count('data'))
+
+	for d in diaDaSemana:
+		porc = (100 * d['valor']) / qtOcorrencias
+		d['nome'] = d['id_dia_semana__dia_da_semana']
+		d['porcentagem'] = porc
+	for m in mes:
+		porc = (100 * d['valor']) / qtOcorrencias
+		m['porcentagem'] = porc
+		m['nome'] = getMes(m['nome'])
+	for h in hora:
+		porc = (100 * d['valor']) / qtOcorrencias
+		h['porcentagem'] = porc
+		h['nome'] = int(h['nome'])
+
+	return render_to_response('mapa_rodovia.html',
+                              RequestContext(request,{'ocorrencias':qtOcorrencias,
+                                                      'mortes': mortes,
+                                                      'diaDaSemana':diaDaSemana,
+                                                      'mes':mes,
+                                                      'hora':hora,
+                                                      'rodovia':rodovia}))
 
 
 def getMes(cod):
