07213b1117b5b64d605e873c57094acd8b39ebfa
Antonio Augusto Foletto <gutofoletto@gmail.com>
2014-10-04 13:53:24 -0300

    adicionado o novo front para o geoliberty

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,135 +1,130 @@
 module.exports = function(grunt) {
 
-	grunt.initConfig({
+    grunt.initConfig({
 
-  	// Project variables
-  	pkg 			: grunt.file.readJSON('package.json'),
-  	sass_dir 		: 'src/sass/',
-  	js_dir 			: 'src/js/',
-  	components_dir 	: 'src/components/',
-  	jquery			: '<%= components_dir %>' + 'jquery/',
-  	bootstrap		: '<%= components_dir %>' + 'sass-bootstrap/',
-    // Banner to be included on compressed and compiled files
-    banner 			: '/**\n' +
-    				  '* <%= pkg.name %> v<%= pkg.version %> \n' +
-    				  '* Copyright <%= grunt.template.today("yyyy") %> <%= pkg.name %> \n' +
-    				  '* @contributors: { \n*    <%= pkg.contributors.join(",\\n*    ") %> \n* }\n' +
-    				  '*/\n',
+        // Project variables
+        pkg: grunt.file.readJSON('package.json'),
 
-    //* Tasks Configurations: *//
 
-    // Compass task will compile the SCSS code to CSS.
-    compass: {
-    	css:{
-    		options: {
-    			config: 'config.rb',
-                specify: ['src/sass/base.scss', 'src/sass/responsive.scss'],
-                noLineComments: true,
-    		}
-    	},
-        bootstrap:{
-            options:{
-                config: 'config.rb',
-                specify: 'src/sass/bootstrap/bootstrap.scss',
-                noLineComments: true,
+        //* Tasks Configurations: *//
+
+        // Compass task will compile the SCSS code to CSS.
+        compass: {
+            css:{
+                options: {
+                    config: 'config.rb',
+                    noLineComments: true
+                }
+            }
+        },
+
+
+        //CSSMin task will minify the main CSS file and put on distribution folder.
+        cssmin: {
+            css: {
+                options: {
+                    report: 'min',
+                    keepSpecialComments: 0
+                },
+                files:{
+                    'assets/css/<%= pkg.name %>.min.css': ['assets/css/truss.css']
+                }
+            }
+        },
+
+
+        // Concat will join JS files into one.
+        concat: {
+            js:{
+                src: 'src/js/*.js',
+                dest: 'assets/js/<%= pkg.name %>.js'
+            }
+        },
+
+
+        // Uglify will minify JS files and put on distribution folder.
+        uglify: {
+            js:{
+                options:{
+                    report: 'min'
+                },
+                files:{
+                    'assets/js/<%= pkg.name %>.min.js': ['assets/js/<%= pkg.name %>.js']
+                }
+            }
+        },
+
+        // Imagemmin will compress images size.
+        imagemin: {
+
+            build: {
+                options:{
+                    optimizationLevel: 5
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'src/img',
+                    src: ['**/*.{png,jpg,gif,ico}'],
+                    dest: 'assets/img'
+                }]
+            }
+        },
+
+        htmlmin: {
+            geoliberty: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: {
+                    'geoliberty/templates/home.html' : 'src/html/home.html',
+                    'geoliberty/templates/base.html' : 'src/html/base.html',
+                    'geoliberty/templates/inicio.html' : 'src/html/inicio.html',
+                    'geoliberty/templates/lista.html' : 'src/html/lista.html',
+                    'geoliberty/templates/mapa.html' : 'src/html/mapa.html',
+                    'geoliberty/templates/regiao_geopolitica.html' : 'src/html/regiao_geopolitica.html',
+                    'geoliberty/templates/regioes_geopoliticas.html' : 'src/html/regioes_geopoliticas.html',
+                }
+            },
+          },
+
+
+        // Watch will monitor changes and run tasks.
+        watch: {
+            html:{
+                files: 'src/html/*.html',
+                tasks: 'htmlmin',
+            },
+            css: {
+                files: 'src/sass/**/*.scss',
+                tasks: ['compass:css', 'cssmin:css']
+            },
+            js: {
+                files: ['src/js/*.js'],
+                tasks: ['concat:js','uglify:js']
+            },
+            img: {
+                files: ['src/img/*.png', 'src/img/*.jpg'],
+                tasks: 'imagemin'
             }
         }
-    },
 
-    // Concat will join CSS and JS files into one.
-    concat: {
-    	css:{
-    		src: [ 'src/css/bootstrap/bootstrap.css', 'src/css/base.css', 'src/css/responsive.css', '!src/css/<%= pkg.name %>.css', 'src/css/chosen.css'],
-    		dest: 'src/css/<%= pkg.name %>.css'
-    	},
-    	js:{
-    		options: {
-    			separator: ';',
-    			banner: '<%= banner %>',
-    		},
-    		src: ['src/js/*.js', '!src/js/<%= pkg.name %>.js', '!src/js/jquery.min.js'],
-    		dest: 'src/js/<%= pkg.name %>.js'
-    	}
-    },
+    });
 
-    //CSSMin task will minify the main CSS file and put on distribution folder.
-    cssmin: {     
-    	css: {
-    		options: {
-    			banner: '<%= banner %>',
-    			keepSpecialComments: '*'
-    		},
-    		files:{
-    			'assets/css/<%= pkg.name %>.min.css': ['src/css/<%= pkg.name %>.css']
-    		}
-    	}
-    },
+    // Loading Tasks
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
 
-    // Copy will copy JS files from bower to src folder.
-    copy: {
-        bootstrap:{
-            files:[
-                { expand: true, flatten: true, src:'<%= bootstrap %>fonts/*.*', dest:'assets/fonts/' },
-                { expand: true, flatten: true, src:'<%= bootstrap %>**/bootstrap.min.js', dest:'src/js/' },
-            ]
-        },
-        jquery:{
-    		files:[
-    		{ expand: true, flatten: true, src:'<%= jquery %>**/jquery.min.js', dest:'assets/js/' },
-    		]
-    	}
-    },
-
-    // Uglify will minify JS files and put on distribution folder.
-    uglify: {
-    	js:{
-    		files:{
-    			'assets/js/<%= pkg.name %>.min.js': ['src/js/<%= pkg.name %>.js'],
-    		}
-    	}
-    },
-
-    // Watch will monitor changes and run tasks.
-    watch: {
-    	css: {
-    		files: ['src/sass/**/*.scss', '!(src/sass/boostrap/*.scss)'],
-    		tasks: ['compass:css', 'concat:css', 'cssmin:css',],
-    	},
-    	bootstrap: {
-    		files: 'src/sass/bootstrap/*.scss',
-    		tasks: ['compass:bootstrap', 'concat:css', 'cssmin:css',]
-    	},
-    	js: {
-    		files: ['src/js/*.js', '!src/js/<%= pkg.name %>.js'],
-    		tasks: ['concat:js','uglify:js',]
-    	},
-    	components:{
-    		files:[
-    		'<%= bootstrap %>**/bootstrap.min.js',
-    		'<%= jquery %>**/jquery.min.js',
-    		'<%= chartjs %>**/Chart.min.js'
-    		],
-    		tasks: ['copy:components',]
-    	}
-    }
-
-});
-
-// Loading Tasks
-grunt.loadNpmTasks('grunt-contrib-compass');
-grunt.loadNpmTasks('grunt-contrib-watch');
-grunt.loadNpmTasks('grunt-contrib-cssmin');
-grunt.loadNpmTasks('grunt-contrib-concat');
-grunt.loadNpmTasks('grunt-contrib-copy');
-grunt.loadNpmTasks('grunt-contrib-uglify');
-
-// Registering Task groups to call with command line
-grunt.registerTask('default', '');  
-grunt.registerTask('build', ['compass', 'copy', 'concat', 'cssmin', 'uglify', ]);
-grunt.registerTask('components', ['copy', 'concat:js', 'uglify']);
-grunt.registerTask('bootstrap', ['compass:bootstrap', 'concat:css', 'cssmin',]);
-grunt.registerTask('css', ['compass:css', 'concat:css', 'cssmin',]);
-grunt.registerTask('js', ['concat:js', 'uglify']);
-grunt.registerTask('dev', ['watch',]);
+    // Registering Task groups to call with command line
+    grunt.registerTask('build', ['compass', 'cssmin', 'concat', 'uglify', 'imagemin', 'htmlmin']);
+    grunt.registerTask('css', ['compass:css', 'cssmin']);
+    grunt.registerTask('js', ['concat:js', 'uglify:js']);
+    grunt.registerTask('dev', 'watch');
+    grunt.registerTask('default', 'build');
 
 };
--- config.rb
+++ config.rb
@@ -1,15 +1,19 @@
 # Require any additional compass plugins here.
-add_import_path "./src/components"
+require 'compass-normalize'
+require 'ceaser-easing'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "src/css"
-sass_dir = "src/sass/"
+css_dir = "assets/css"
+sass_dir = "src/sass"
 images_dir = "src/img"
 javascripts_dir = "src/js"
 
-output_style = :expanded # :expanded or :nested or :compact or :compressed
+# You can select your preferred output style here (can be overridden via the command line):
+output_style = :expanded #:expanded or :nested or :compact or :compressed
 
+# To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
 
+# To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = false
